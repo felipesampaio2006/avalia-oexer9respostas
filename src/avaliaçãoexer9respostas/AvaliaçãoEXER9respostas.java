cb825d53b5ef095be43e694e86a44f22aad0ac7f
felipe Sampaio <samlipe2006@gmail.com>
2021-07-01 16:34:34 -0300

    atividade da avaliação feito dia 01/07/2021

---
+++ src/avaliaçãoexer9respostas/AvaliaçãoEXER9respostas.java
@@ -0,0 +1,25 @@
+package avaliaçãoexer9respostas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Felipe
+ * Escreva um programa que imprima todos os números inteiros de 100 a 1 (em ordem
+decrescente),  mostre a soma dos valores de 100 a 1 e quantos pares existem neste intervalo.
+ */
+public class AvaliaçãoEXER9respostas {
+    public static void main(String[] args) {
+        int cont;
+        Scanner ler = new Scanner(System.in);
+        cont = 1;
+        while(cont <= 100){
+            if(cont == 1) {
+                
+            }
+        }
+        cont--;
+    }
+    
+}
+
